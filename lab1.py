d5c0ca588558f4f555423d8398a3df181ed81d0d
Julian Saldana <julianesaldana@yahoo.com>
2021-01-29 12:24:23 -0800

    added more to lab1

--- lab1.py
+++ lab1.py
@@ -2,35 +2,21 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-# creating numpy array
-firstDie = np.zeros((100000, 1))
-secondDie = np.zeros((100000, 1))
+list_Prob = []
+list_Obj = []
+for i in range(60):
+    x = pow((5 / 6), (i - 1)) * (1 / 6)
+    list_Prob.insert(i + 1, x)
 
-totalExperiments = 0
-counter = 0
-while totalExperiments < 100000:  # will do experiment n times
-    for j in range(60):  # will only add experiments that add to 7 within 60 rolls
-        counter += 1
-        first = random.randint(1, 6)
-        second = random.randint(1, 6)
-        if first + second == 7:
-            firstDie[totalExperiments, :] = first
-            secondDie[totalExperiments, :] = second
-            totalExperiments += 1
-            counter = 0
-            break
+for j in range(60):
+    list_Obj.insert(j, j + 1)
 
-count = firstDie + secondDie
+y_pos = np.arange(len(list_Obj))
+performance = list_Prob
 
-x_axis = range(1, 61)
-sb = np.size(x_axis)
-h1, bin_edges = np.histogram(count, x_axis)
-b1 = bin_edges[0:sb - 1]
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, list_Obj)
+plt.ylabel('P(X=x)')
+plt.title('Probability Histogram')
 
-#
-fig1 = plt.figure(1)
-plt.stem(b1, h1)
-plt.title('Stem plot - Sum of two dice')
-plt.xlabel('Number of tries it takes to roll 7 total')
-plt.ylabel('Number of occurrences')
-fig1.savefig('Sum of two dice.jpg')
+plt.show()
